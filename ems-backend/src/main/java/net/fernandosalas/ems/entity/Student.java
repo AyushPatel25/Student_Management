74047e3a730855d38b4be9027053c48df17bbf3a
AyushPatel25 <ayushpatel250204@gmail.com>
2024-03-23 12:22:49 +0530

    feat : commit successfully

--- ems-backend/src/main/java/net/fernandosalas/ems/entity/Student.java
+++ ems-backend/src/main/java/net/fernandosalas/ems/entity/Student.java
@@ -16,7 +16,7 @@ public class Student {
     private Long id;
 
     @Column(name = "first_name")
-    private String firstName;
+    private String fName;
 
     @Column(name = "last_name")
     private String lastName;
